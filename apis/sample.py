eac7c179844c6459c1804355af94285e27d575ff
Vinu Ramanan <vinufutureastronaut@gmail.com>
2020-11-02 00:04:47 +0530

    changed

--- apis/sample.py
+++ apis/sample.py
@@ -2,8 +2,8 @@ from flask_restplus import Namespace, Resource
 
 namespace = Namespace('sample', description='Sample API to test Jenkins')
 
-@namespace.route('message')
+@namespace.route('/message/{name}')
 class Message(Resource):
     @namespace.doc(description='Greetiings')
-    def get(self):
-        return 'Hello Budraan!'
+    def get(self, name):
+        return 'Hello {}!'.format(name)
